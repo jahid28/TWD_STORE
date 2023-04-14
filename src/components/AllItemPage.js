f17ac2f3aacde296ebd518132f74f4486a0ddee9
Jahid Khan <connect2jahidkhan@gmail.com>
2023-04-15 00:24:24 +0530

    changed

--- src/components/AllItemPage.js
+++ src/components/AllItemPage.js
@@ -1,8 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, CSSProperties } from 'react'
+import PulseLoader from "react-spinners/PulseLoader";
+
 import axios from 'axios';
 import LoadingBar from 'react-top-loading-bar'
 import { ToastContainer, toast } from 'react-toastify';
 import ItemCard from './ItemCard.js';
+
+// const override: CSSProperties = {
+//     display: "block",
+//     margin: "0 auto",
+//     borderColor: "red",
+//   };
 
 export default function AllItemPage(props) {
     const [progress, setProgress] = useState(0)
@@ -10,12 +18,13 @@ export default function AllItemPage(props) {
     const [selectedOption, setSelectedOption] = useState('All');
     const [totalPages, setTotalPages] = useState(0)
     const [pageNum, setPageNum] = useState(1)
+    let [loading, setLoading] = useState(true);
 
-    const URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:8000'
+
+    // const URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:8000'
 
 
     const typeChange = (event) => {
-        event.preventDefault()
 
         setSelectedOption(event.target.value);
 
@@ -25,27 +34,31 @@ export default function AllItemPage(props) {
 
     const typeChangeSubmit = async (e) => {
         setProgress(20)
+        setLoading(true)
+
 
 
         try {
 
-            await axios.post(`${URL}/getProducts`, {
+            await axios.post(`${process.env.REACT_APP_SERVER_URL}/getProducts`, {
                 selectedOption
             })
                 .then(res => {
-                    setProgress(50)
-                    const x = res.data
-                    setData(x.allProducts)
-                    setTotalPages(Math.ceil((x.totalItems) / 12))
-                    setPageNum(1)
-                    setProgress(100)
-                    // if(res.data=="jk"){
-                    //     toast.success('jk')
-                    // }
+                    if (res.data == 'fail') {
+                        toast.error("Somethig went wrong!")
 
-                    // else{
-                    //     toast.success('nothing')
-                    // }
+                    }
+                    else {
+                        setProgress(50)
+                        const x = res.data
+                        setData(x.allProducts)
+                        setTotalPages(Math.ceil((x.totalItems) / 12))
+                        setPageNum(1)
+                        setProgress(100)
+                    }
+                    setLoading(false)
+
+
                 })
                 .catch(e => {
                     toast.error("Somethig went wrong!");
@@ -72,6 +85,7 @@ export default function AllItemPage(props) {
 
     const pageChangeSubmit = async (e) => {
         setProgress(20)
+        setLoading(true)
 
 
         try {
@@ -83,6 +97,8 @@ export default function AllItemPage(props) {
                     setProgress(50)
                     setData(res.data)
                     setProgress(100)
+                    setLoading(false)
+
 
                 })
                 .catch(e => {
@@ -114,65 +130,62 @@ export default function AllItemPage(props) {
 
 
     return (
-        <div>
 
-            <LoadingBar
-                color='red'
-                progress={progress}
-                onLoaderFinished={() => setProgress(0)}
-            />
+        data.length > 0 ?
+            (
+                <div>
 
-            <h2 className='font-bold text-base my-2 ml-3 lg:text-3xl lg:my-4' >Search by Categories : </h2>
-            <select className='border border-black rounded mb-2 ml-3 lg:text-xl' value={selectedOption} onChange={typeChange} >
-                <option >All</option>
-                <option >Electronics</option>
-                <option >Fashion</option>
-                <option >Toys</option>
-                <option >Sports</option>
-                <option >Kitchen</option>
-            </select>
+                    <LoadingBar
+                        color='red'
+                        progress={progress}
+                        onLoaderFinished={() => setProgress(0)}
+                    />
 
-            {/* <h1>count is {count}</h1> */}
+                    <h2 className='font-bold text-base my-2 ml-3 lg:text-3xl lg:my-4' >Search by Categories : </h2>
+                    <select className='border border-black rounded mb-2 ml-3 lg:text-xl' value={selectedOption} onChange={typeChange} >
+                        <option >All</option>
+                        <option >Electronics</option>
+                        <option >Fashion</option>
+                        <option >Toys</option>
+                        <option >Sports</option>
+                        <option >Kitchen</option>
+                    </select>
 
+                    <section className="text-gray-600 body-font ">
+                        <div className="container px-5 py-12 mx-auto  ">
 
-            <section className="text-gray-600 body-font ">
-                <div className="container px-5 py-12 mx-auto  ">
-
-                    <div className="flex flex-wrap -m-4  ">
-
-                        {
-
-                            data.length > 0 ? (
-                                data.map((item) => (<div className='lg:w-1/4 md:w-1/2 p-2 scale-95 my-4 w-full  rounded-lg shadow-md cursor-pointer hover:shadow-2xl' key={item._id}><ItemCard name={item.name} type={item.type} price={item.price} stocks={item.stocks} img={item.img} allRatings={item.allRatings} reviews={item.reviews} /></div>))
-                            )
-
-                                : <div>Loading...</div>
-
-                        }
+                            <div className="flex flex-wrap -m-4  ">
+                                {
+                                    data.map((item) => (<div className='lg:w-1/4 md:w-1/2 p-2 scale-95 my-4 w-full  rounded-lg shadow-md cursor-pointer hover:shadow-2xl' key={item._id}><ItemCard name={item.name} type={item.type} price={item.price} stocks={item.stocks} img={item.img} allRatings={item.allRatings} reviews={item.reviews} /></div>))
+                                }
+                            </div>
+                        </div>
+                    </section>
 
 
+
+
+
+                    <div id="buttons" class="flex justify-center mb-10 items-center">
+                        <button disabled={pageNum <= 1 ? true : false} onClick={prev} class="mr-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Previous</button>
+                        <p>Page : {pageNum}/{totalPages}</p>
+                        <button disabled={pageNum >= totalPages ? true : false} onClick={next} class="ml-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Next</button>
                     </div>
+
+
+
                 </div>
-            </section>
-
-
-
-            {data.length > 0 ?
-
-                <div id="buttons" class="flex justify-center mb-10 items-center">
-                    <button disabled={pageNum <= 1 ? true : false} onClick={prev} class="mr-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Previous</button>
-                    <p>Page : {pageNum}/{totalPages}</p>
-                    <button disabled={pageNum >= totalPages ? true : false} onClick={next} class="ml-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Next</button>
-                </div>
-
-                :
-
-                <div>Loding...</div>
-
-            }
-
-
-        </div>
+            )
+            :
+            <div className='flex justify-center w-full'>
+                <PulseLoader
+                    color='rgb(74, 87, 224)'
+                    loading={loading}
+                    size={20}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+            </div>
 
 
     )
